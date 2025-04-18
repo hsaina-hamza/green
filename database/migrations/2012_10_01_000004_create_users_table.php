6a3a763f24efad3f2f7a2b94a760be2759b31917
Hamza hsaina <hhsaina4@gmail.com>
2025-04-18 03:55:25 +0200

    2

--- database/migrations/2012_10_01_000004_create_users_table.php
+++ database/migrations/2012_10_01_000004_create_users_table.php
@@ -4,24 +4,35 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['admin', 'worker', 'user'])->default('user');
-            $table->rememberToken();
+            $table->date('date_of_birth')->nullable();
+            $table->enum('gender', ['male', 'female', 'other']);
+            $table->enum('role', ['admin', 'user', 'moderator']);
             $table->timestamps();
+            $table->timestamp('last_login_at')->nullable();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
         Schema::dropIfExists('users');
     }
-};
+}
